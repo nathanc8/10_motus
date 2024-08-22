b0a80199456e1b30c18ad9941442cda1eee0b814
nathanc8 <cazard.nathan@gmail.com>
2024-08-22 16:19:10 +0200

    ajustement pour rendre le jeu fiable

--- script.js
+++ script.js
@@ -9,28 +9,28 @@ function tryWord(word, base) {
     wellPlaced = [];
     notInWord = [];
     missplaced = [];
-    /* if (word === base) {
-        wellPlaced = word.split("");
-        return { wellPlaced, missplaced, notInWord };
-    } else { */
+
     let arrayBase = base.split("");
     let arrayWord = word.split("");
+    //Vérifie si le mot est un match parfait
     if (word === base) {
         wellPlaced = word.split("");
     } else {
+        //Vérifie si certains charactères sont parfaitement placés, les push dans le tableau wellPlaced et remplacent ceux qui correspondent à ce critère par une valeur null
         arrayWord.forEach((char, index) => {
             if (char === arrayBase[index]) {
                 wellPlaced.push(char);
                 arrayBase[index] = null;
             }
         });
-
+        //Vérifie si certains charactères sont correctes, mais mal placés, les push dans le tableau missPlaced et retire ceux qui correspondent à ce critère par une valeur null.
         arrayWord.forEach((char, index) => {
             if (arrayBase.includes(char)) {
                 if (char !== arrayBase[index]) {
                     missplaced.push(char);
                     arrayBase[index] = null;
                 }
+                //Vérifie si certains charactères sont incorrectes, et les push dans le tableau notInWord
             } else if (!arrayBase.includes(char)) {
                 notInWord.push(char);
             }
@@ -41,15 +41,16 @@ function tryWord(word, base) {
         return { wellPlaced, missplaced, notInWord };
     }
 }
+
 function guess() {
     let base = "dictionnaire";
     let word = document.getElementById("word").value;
-    let result = tryWord(word, base);
+    tryWord(word, base);
     document.getElementById("word").value = "";
     document.getElementById("try").innerText = word;
-    document.getElementById("well").innerText = "Bien placé: " + result.wellPlaced.join(", ");
-    document.getElementById("miss").innerText = "Mal placé: " + result.missplaced.join(", ");
-    document.getElementById("not").innerText = "Pas dans le mot: " + result.notInWord.join(", ");
+    document.getElementById("well").innerText = "Bien placé: " + wellPlaced.join(", ");
+    document.getElementById("miss").innerText = "Mal placé: " + missplaced.join(", ");
+    document.getElementById("not").innerText = "Pas dans le mot: " + notInWord.join(", ");
     if (wellPlaced.length === base.length) {
         document.getElementById("win").innerText = "Vous avez gagné";
     }
